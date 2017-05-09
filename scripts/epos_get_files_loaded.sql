e6bebff574b945e15255bfcd2b2c07d91296ab74
colathurv <colathurv@yahoo.com>
2017-05-09 01:14:34 -0700

    First Commit

--- scripts/epos_get_files_loaded.sql
+++
@@ -1,3 +0,0 @@
-select distinct(trans_date), store_code, substr(trans_no,instr(trans_no,'-') + 1,8)
-from srcm.srcm_nbks_trans_summary
-group by store_code , trans_date
--- scripts/srcm_user_creation.sql
+++
@@ -1,8 +0,0 @@
--- Go as MYSQL Root user to create the srcm user 
-USE mysql;
-show databases ;
-create database srcm ;
-create user 'srcmadmin' identified by '<Enter the password of this user>' ;
-grant all on srcm.* to srcmadmin ;
-show grants for 'srcmadmin' ;
-
